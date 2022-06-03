de1e77f36399769eeb9cd710abbd052bc7e398f7
Eric S. Londres <ericlondres@outlook.com>
2022-06-02 20:16:24 -0400

    Release as GNU AGPL 3.0

--- lib/diarchy/application.ex
+++ lib/diarchy/application.ex
@@ -1,3 +1,12 @@
+## This file is part of Diarchy, a concurrent spartan server.
+## Copyright 2022 Eric S. Londres
+## This program is free software: you can redistribute it and/or modiy it under the terms of the GNU Affero General
+##  Public License as published by the Free Software Foundation, version 3. This program is distributed in the hope
+##  that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+##  FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more details.
+## You should have received a copy of the GNU Affero General Public License along with this program.
+##  If not, see <https://www.gnu.org/licenses/>.
+
 defmodule Diarchy.Application do
   # See https://hexdocs.pm/elixir/Application.html
   # for more information on OTP Applications
--- lib/diarchy/server.ex
+++ lib/diarchy/server.ex
@@ -1,3 +1,12 @@
+## This file is part of Diarchy, a concurrent spartan server.
+## Copyright 2022 Eric S. Londres
+## This program is free software: you can redistribute it and/or modiy it under the terms of the GNU Affero General
+##  Public License as published by the Free Software Foundation, version 3. This program is distributed in the hope
+##  that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+##  FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more details.
+## You should have received a copy of the GNU Affero General Public License along with this program.
+##  If not, see <https://www.gnu.org/licenses/>.
+
 defmodule Diarchy.Server do
   @moduledoc "Network interface code"
 
